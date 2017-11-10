bee77ee9104a9a1b2f1de0f09962af9e5fc1c022
LiShitong <lidalanlah5@163.com>
2017-11-10 14:23:08 +0800

    第一次合并，难

--- doubanmovie/src/pages/FindMovie.vue
+++ doubanmovie/src/pages/FindMovie.vue
@@ -1,21 +1,18 @@
 <template lang="html">
   <div class="">
-    <TabBar></TabBar>
     <remen></remen>
     <top250></top250>
-    <TabBar></TabBar>
   </div>
 </template>
 
 <script>
 import remen from './remen'
 import top250 from './top250'
-import TabBar from './TabBar'
 
   export default {
     components:{
       remen,
-      top250,TabBar
+      top250
     }
   }
 </script>
