c91ba2e29c81a972f7d111abc2165715c620a27d
zhaihuimin <15137277594@163.com>
2017-11-10 20:33:24 +0800

    我的模块一级页面完成

--- doubanmovie/src/pages/Mine.vue
+++ doubanmovie/src/pages/Mine.vue
@@ -1,17 +1,100 @@
 <template lang="html">
-    <div>
-      <p>我的</p>   
-    </div> 
+
+  <div class="">
+    <div class="top">
+      <img class="shezhi" src="../../static/shezhi.png" alt="">
+      <div class="login">
+        <img class="loginImg" src="../../static/login.png" alt="">
+        <span id="loginSpan">未登录</span>
+      </div>
+    </div>
+
+
+      <div class="Sees">
+        <router-link :class="{'router-link-active':ok}"   class="  See" to='/Mine/wantSee'>想看</router-link>
+        <router-link class="See" to="/Mine/nowSee">在看</router-link>
+        <router-link class="See" to="/Mine/sew">看过</router-link>
+        <router-link class="See" to="/Mine/seeSpeak">影评</router-link>
+        <router-link class="See" to="/Mine/movieMen">影人</router-link>
+      </div>
+      <router-view></router-view>
+
+
+
+</div>
 </template>
 
 <script>
   export default {
-    props: ['show']
+    props: ['show'],
+    data(){
+      return {
+        ok:true
+      }
+  },
+  methods:{
+
+
+  },
+  watch:{
+    '$route'(newValue,oldValue){
+    this.ok=false;
+     
+    }
+  }
+
+  // props: ['show']
   }
 </script>
 
 
 <style lang="less" scoped>
+.shezhi{
+      width:.4rem;
+      height:.4rem;
+      float:right;
+      padding:.4rem;
+    }
+  .top{
+    width: 100%;
+    height:3rem;
+    background:url('../../static/dbminebg.png')  no-repeat;
+    background-size: 100% 3rem;
+  }
+  .login{
+    padding:.8rem;
+  }
+  .loginImg{
+    width:.8rem;
+    height:.8rem;
+    vertical-align:middle;
+  }
+  #loginSpan {
+    font-size:.3rem;
+    color:white;
+    font-wight:800;
+    padding:.1rem;
+    vertical-align:middle;
+  }
+  .Sees{
+    display: flex;
+    width:100%;
+    justify-content: space-around;
+    font-size:.3rem;
+    color:#432443;
+  }
+  .Sees .See{
+      margin-top:.2rem;
+      color:gray;
+      display:inline-block;
+      padding-bottom:.2rem;
 
+  }
+  .Seemoren{
+    // border-bottom:.02rem solid #666;
+  }
+  .router-link-active{
+      color:black!important;
+      border-bottom:.02rem solid black!important;
+  }
 </style>
-
--- doubanmovie/src/router/index.js
+++ doubanmovie/src/router/index.js
@@ -3,7 +3,11 @@ import Router from 'vue-router'
 import HotPlay from '../pages/HotPlay'
 import FindMovie from '../pages/FindMovie'
 import Mine from '../pages/Mine'
-
+import wantSee from '../pages/wantSee'
+import nowSee from '../pages/nowSee'
+import sew from '../pages/sew'
+import seeSpeak from'../pages/seeSpeak'
+import movieMen from'../pages/movieMen'
 
 Vue.use(Router)
 
@@ -11,7 +15,14 @@ const routes = [
     { path: '/', component: HotPlay },
     { path: '/HotPlay', component: HotPlay },
     { path: '/FindMovie', component: FindMovie },
-    { path: '/Mine', component: Mine },
+    { path: '/Mine', component: Mine,
+    children:[{path:'/',component:wantSee},
+      {path:'/Mine/wantSee',component:wantSee},
+    {path:'/Mine/nowSee',component:nowSee},
+    {path:'/Mine/sew',component:sew},
+    {path:'/Mine/seeSpeak',component:seeSpeak},
+    {path:'/Mine/movieMen',component:movieMen}]
+     }
 ]
 
 const router = new Router({
