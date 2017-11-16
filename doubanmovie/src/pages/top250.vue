a39522c38385c0a63f3b49191f3187e6be515877
WangYu <1192591724@qq.com>
2017-11-16 15:37:58 +0800

    ...

--- doubanmovie/src/pages/top250.vue
+++ doubanmovie/src/pages/top250.vue
@@ -36,7 +36,6 @@ export default {
           autoplay: false,
           observer:true,//修改swiper自己或子元素时，自动初始化swiper
           observeParents:true,//修改swiper的父元素时，自动初始化swiper
-          
           onSlideChangeEnd:(swiper) => {
             if(swiper.activeIndex == 0){
               this.title.title = this.dat1.tit;
